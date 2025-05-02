88664502fb8710cfcc2816327cd484e4decbe586
route250@mx130 <hanaya.white@gmail.com>
2025-05-02 10:38:21 +0900

    voskモデルを整理

--- fastrtc_jp/speech_to_text/vosk.py
+++ fastrtc_jp/speech_to_text/vosk.py
@@ -1,25 +1,35 @@
 import sys,os
+import json
 from typing import Any
-from fastrtc.speech_to_text.stt_ import STTModel
 import numpy as np
 from numpy.typing import NDArray
+from fastrtc.speech_to_text.stt_ import STTModel
+from fastrtc.utils import audio_to_float32, audio_to_int16
+from fastrtc_jp.speech_to_text.util import resample_audio
 import vosk
-import json
+
+# 参考までにモデルの保存先
+# MODEL_DIRS = [os.getenv("VOSK_MODEL_PATH"), Path("/usr/share/vosk"),
+#        Path.home() / "AppData/Local/vosk", Path.home() / ".cache/vosk"]
 
 class VoskSTT(STTModel):
     """fastrtcのSTTModelを実装したクラス"""
-    def __init__(self):
+    def __init__(self,model_name:str|None=None,lang:str|None=None):
         vosk.SetLogLevel(-1)
         #self.model = vosk.Model(model_name="vosk-model-small-ja-0.22")
-        self.model = vosk.Model(model_name="vosk-model-ja-0.22")
+        if not model_name and not lang:
+            model_name = "vosk-model-ja-0.22"
+        self.model = vosk.Model(model_name=model_name,lang=lang)
 
     def stt(self, audiodata: tuple[int, NDArray[np.int16 | np.float32]]) -> str:
         sample_rate,audio_streo = audiodata
         print(f"Received audio: sr:{sample_rate} {audio_streo.shape} {audio_streo.dtype}")
-        audio_mono:NDArray = audio_streo[0]
-        # audiof = audio_stereo[0].astype(np.float32) / 32768.0
-        audio_bin = audio_mono.tobytes() # struct.pack('<' + 'h'*len(audio_mono), *audio_mono)
-
+        audio_mono = audio_streo[0]
+        # if sample_rate != 160000:
+        #     audio_mono = audio_to_float32(audio_mono)
+        #     audio_mono = resample_audio(sample_rate, audio_mono, 16000)
+        audio_mono = audio_to_int16(audio_mono)
+        audio_bin = audio_mono.tobytes()
         rec = vosk.KaldiRecognizer(self.model, sample_rate)
         rec.AcceptWaveform(audio_bin)
         result_str = rec.FinalResult()
---
+++ tests/test_vosk.py
@@ -0,0 +1,39 @@
+import sys,os,asyncio
+sys.path.insert(0,'./')
+import time
+from typing import Any
+import numpy as np
+
+import wave
+import importlib.resources as resources
+from fastrtc_jp.speech_to_text.vosk import VoskSTT
+
+def test_english():
+    """英語の音声認識テスト"""
+    #Kokoro is an open-weight TTS model with 82 million parameters. Despite its lightweight architecture, it delivers comparable quality to larger models while being significantly faster and more cost-efficient. With Apache-licensed weights, Kokoro can be deployed anywhere from production environments to personal projects.
+    test_wave_file = resources.files('fastrtc.speech_to_text').joinpath('test_file.wav')
+    print(f"test {test_wave_file}")
+    with wave.open(str(test_wave_file), 'rb') as wf:
+        sample_rate = wf.getframerate()
+        audio_i16 = np.frombuffer(wf.readframes(wf.getnframes()), dtype=np.int16)
+    stt = VoskSTT(model_name='vosk-model-small-en-us-0.15')
+    audio_in = (sample_rate, audio_i16.reshape(1,-1))
+    stt_result = stt.stt(audio_in)
+    print(f"stt: {stt_result}")
+
+def test_japanese():
+    test_wave_file = 'tests/testData/voice_mosimosi.wav'
+    print(f"test {test_wave_file}")
+    with wave.open(str(test_wave_file), 'rb') as wf:
+        sample_rate = wf.getframerate()
+        audio_i16 = np.frombuffer(wf.readframes(wf.getnframes()), dtype=np.int16)
+    stt = VoskSTT()
+    audio_in = (sample_rate, audio_i16.reshape(1,-1))
+    t1 = time.time()
+    stt_result = stt.stt(audio_in)
+    t2 = time.time()
+    print(f"stt: {stt_result} {t2-t1}(sec)")
+
+if __name__ == "__main__":
+    test_english()
+    test_japanese()
